2fe5b4c63a9d7e843b72de99ad02fdd6b91d0c8d
Hxnriquezin <henriquedovalestankowicz@gmail]>
2024-10-29 14:42:07 -0300

    app.py

--- app.py
+++ app.py
@@ -3,5 +3,8 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('site.html')
+@app.route('/site')
+def site():
+    return render_template('site.html')
 if __name__ =='__main__':
     app.run(debug=True)

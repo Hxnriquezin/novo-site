32607d26b4d1439ce9d7de03fd78ff6ec4cb4935
Hxnriquezin <henriquedovalestankowicz@gmail.com>
2024-11-07 14:36:06 -0300

    git

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask,render_template,request
 app = Flask(__name__)
+
 @app.route('/')
 def home():
     return render_template('site.html')
-@app.route('/site')
-def site():
-    return render_template('site.html')
-if __name__ =='__main__':
+
+
+@app.route ('/login')
+def login():
+    return render_template('login.html')
+
+if __name__ =='__main__': 
     app.run(debug=True)
